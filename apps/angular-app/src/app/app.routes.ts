415c6ca1d8faded3d2349f8da164288c583273cd
K1NG-Devops <61783617@mylife.unisa.ac.za>
2025-04-28 16:43:31 +0200

    fixed angular config files

--- apps/angular-app/src/app/app.routes.ts
+++ apps/angular-app/src/app/app.routes.ts
@@ -1,9 +1,3 @@
 import { Routes } from '@angular/router';
-import { AdmissionComponent } from './pages/admission/admission.component';
-import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [
-  { path: 'admission', component: AdmissionComponent },
-  { path: '', redirectTo: 'admission', pathMatch: 'full' },
-  {path:'login', component: LoginComponent}
-];
+export const routes: Routes = [];
