0af1d22de2f1f8ce8176fe7b14b6b7875bb47bd5
reeri <precious@youngeagles.org.za>
2025-04-29 15:43:41 +0200

    Added landing page

--- apps/angular-app/src/app/app.routes.ts
+++ apps/angular-app/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { FormComponent } from './pages/form/form.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { LandingComponent } from './pages/landing/landing.component';
 
 export const routes: Routes = [
 
 {path: 'login', component: LoginComponent},
 {path:'form', component: FormComponent},
-{path: 'register', component: RegisterComponent}
+{path: 'register', component: RegisterComponent},
+{path: 'landing', component: LandingComponent},
 ];
