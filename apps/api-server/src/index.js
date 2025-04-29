039b19830b6a93f499f21d29ec7490a70d50c358
K1NG-Devops <61783617@mylife.unisa.ac.za>
2025-04-29 18:24:05 +0200

    Setup routing in auth.routs.js, setup db, connected server to db

--- apps/api-server/src/index.js
+++ apps/api-server/src/index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +11,8 @@ app.use(express.json());
 app.get('/api', (req, res) => {
   res.json({ message: 'API is running' });
 });
+
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
   console.log(`API server is running on http://localhost:${port}`);
