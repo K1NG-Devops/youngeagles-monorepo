2bb1a663a9305d5140b3f6ca0110c70f92f5280c
K1NG-Devops <61783617@mylife.unisa.ac.za>
2025-04-30 00:55:22 +0200

    Created: .env, updated db.js to load dotenv generated pnpm-lock.yaml file inside api-server package.json

--- apps/api-server/src/db.js
+++ apps/api-server/src/db.js
@@ -1,14 +1,16 @@
 import mysql from 'mysql2/promise'; // Import mysql2/promise for promise-based MySQL queries
-// import dotenv from 'dotenv'; // Import dotenv to load environment variables
+import dotenv from 'dotenv'; // Import dotenv to load environment variables
+
+dotenv.config(); // Load environment variables from .env file
 
 const pool = mysql.createPool({
-    host: 'localhost', // Database host
-    user: 'root', // Database user
-    password: '#Olivia@17', // Database password
-    database: 'skydek_auth', // Database name
-    waitForConnections: true, // Wait for connections
-    connectionLimit: 10, // Maximum number of connections
-    queueLimit: 0, // No limit on the queue
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
 });
 
 export default pool; // Export the connection pool for use in other modules
